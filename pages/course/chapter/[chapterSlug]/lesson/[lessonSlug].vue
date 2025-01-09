73760c67b03d2126c0d5586bb546c065b8c046e6
nathanmbicho <nathanmbicho@gmail.com>
2025-01-10 02:22:01 +0300

    dynamic routing in nuxt

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,24 @@
+<script setup>
+    const course = useCourse();
+    const route = useRoute();
+
+    const chapter = computed(() => {
+        return course.chapters.find(
+            (chapter) => chapter.slug === route.params.chapterSlug
+        )
+    })
+
+    const lesson = computed(() => {
+        return chapter.value.lessons.find(
+            (lesson) => lesson.slug === route.params.lessonSlug
+        )
+    })
+</script>
+<template>
+    <h2>Lesson</h2>
+    <p>This is a lesson</p>
+    <ul>
+        <li>{{ chapter.title }}</li>
+        <li>{{ lesson.title }}</li>
+    </ul>
+</template>
